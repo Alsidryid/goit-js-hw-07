effa40f753742fa32fe286593dc5bd3acb8d4f48
Aleksey S <alsidryid@gmail.com>
2022-05-21 16:21:20 +0300

    add content

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery")
+
+const addImages = galleryItems
+.map((item) => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+)
+    .join('')
+gallery.insertAdjacentHTML("beforeend", addImages)
+
+
+const openModal = (e) => {
+
+    e.preventDefault()
+    if(e.target.dataset.source){
+ 
+        basicLightbox.create(`
+		<img width="100%"  src="${e.target.dataset.source}">
+	`).show()
+    }
+  
+}
+gallery.addEventListener("click", openModal);
+
+
+
+
+function closeModal() {
+    const modalWindow = document.querySelector('.basicLightbox');
+    modalWindow.remove();    
+}
+
+function closeModalOnEscKeyClick(evt) {
+  if (evt.keyCode === 27) {
+      closeModal();  
+  }
+}
+window.addEventListener('keydown', closeModalOnEscKeyClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery")
+
+const addImages = galleryItems
+.map((item) => `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`)
+    .join('')
+gallery.insertAdjacentHTML("beforeend", addImages)
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+
+
+
+
+const openModal = (e) => {
+    e.preventDefault()
+    if(e.target.nodeName ==="IMG"){
+      const lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay:250 });
+    }
+  
+}
+
+
+gallery.addEventListener("click", openModal)
